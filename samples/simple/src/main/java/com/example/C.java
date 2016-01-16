29a034e5075acea0cda8d8e5ab2bc6dc7b0cbb05
Soichiro Kashima <soichiro.kashima@gmail.com>
2016-01-16 15:29:49 +0900

    Add example for failed test by exception

--- samples/simple/src/main/java/com/example/C.java
+++ samples/simple/src/main/java/com/example/C.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import java.lang.RuntimeException;
+
 public class C {
     public String greet() {
+        if (true) {
+            throw new RuntimeException("This exception should break the test.");
+        }
         return "Good morning";
     }
 }
