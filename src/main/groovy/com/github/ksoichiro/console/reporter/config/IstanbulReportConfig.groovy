2307e6a2ef03a4af06ef83d6fdb29004db61a1d3
Jeffrey Walraven <jeff.walraven@bti360.com>
2017-07-11 13:43:32 -0400

    Add logging error and set better default

--- src/main/groovy/com/github/ksoichiro/console/reporter/config/IstanbulReportConfig.groovy
+++ src/main/groovy/com/github/ksoichiro/console/reporter/config/IstanbulReportConfig.groovy
@@ -5,5 +5,6 @@ import com.github.ksoichiro.console.reporter.report.IstanbulReport
 class IstanbulReportConfig extends CoverageReportConfig<IstanbulReport> {
     IstanbulReportConfig() {
         boolean enabled = true
+        onlyWhenCoverageTaskExecuted = false
     }
 }
--- src/main/groovy/com/github/ksoichiro/console/reporter/parser/IstanbulReportParser.groovy
+++ src/main/groovy/com/github/ksoichiro/console/reporter/parser/IstanbulReportParser.groovy
@@ -4,8 +4,13 @@ import com.github.ksoichiro.console.reporter.config.IstanbulReportConfig
 import com.github.ksoichiro.console.reporter.report.IstanbulReport
 import groovy.json.JsonSlurper
 import org.gradle.api.Project
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class IstanbulReportParser implements ReportParser<IstanbulReport, IstanbulReportConfig> {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass())
+
     @Override
     IstanbulReport parse(Project project, IstanbulReportConfig config) {
         IstanbulReport report = new IstanbulReport()
@@ -14,6 +19,7 @@ class IstanbulReportParser implements ReportParser<IstanbulReport, IstanbulRepor
         }
         def testReportFile = config.reportFile ?: project.file("${project.projectDir}/coverage/coverage-summary.json")
         if (!testReportFile.exists()) {
+            log.error("Istanbul coverage summary file not found. Return empty report.")
             return report
         }
         def rootNode = new JsonSlurper().parseText(testReportFile.text)
